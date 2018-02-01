5d4fdad2b4ce444e163091801fbed4cab370b2af
axzie <axel.zieschank@student.uni-halle.de>
2018-02-01 11:44:12 +0100

    real update of createSitesMat

--- createSitesMat.R
+++ createSitesMat.R
@@ -1,14 +1,15 @@
 #TODO: should we crop?
 
-library("dplyr")
-library("tidyr")
+
 
 #creat sitesXspecies - matrix
 #input: vector: species, plot_ids
 #output: dataframe containing the matrix
 
-create_sites_species_m <- function(species,plot_ids)
+create_sites_species_m <- function(data)
 {
+  library("dplyr")
+  library("tidyr")
   result <- data %>% group_by(plot.id)  %>% count(spr1) %>% spread(spr1,n)
   result[is.na(result)] <- 0
   result$plot.id<-NULL
@@ -29,9 +30,8 @@ give_sites_species_matrix_list <- function(the_list,index=NULL)
       selection <- the_list[index]
       result <- lapply(selection,function(x)
       {
-        species <- x[,7]
-        plot_ids <- x[,4]
-        create_sites_species_m(species,plot_ids)
+        data <- x[,c(7,4)]
+        return(create_sites_species_m(data))
       })
       return(result)
     } 
@@ -41,14 +41,14 @@ give_sites_species_matrix_list <- function(the_list,index=NULL)
   {
     result <- lapply(the_list,function(x)
     {
-      species <- x[,7]
-      plot_ids <- x[,4]
-      create_sites_species_m(species,plot_ids)
+      data <- x[,c(7,4)]
+      return(create_sites_species_m(data))
     })
     return(result)
   }
 }
 
-test <- give_sites_species_matrix_list(sim.neutral,1:100)
+test <- give_sites_species_matrix_list(sim.neutral,c(1))
+test2 <- give_sites_species_matrix_list(sim.niche,c(1))
 
 install.packages("")
